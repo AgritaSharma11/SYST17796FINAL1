aee1d491e85f6db5eaaa5f6557d2f229257956d7
agrit <agrit@10.0.0.61>
2021-04-20 10:50:20 -0400

    Average Class created and tested

---
+++ Final_Exam/src/final_exam/Average.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package final_exam;
+
+/**
+ *
+ * @author agrit
+ */
+public class Average {
+    public double calculateAverage(double num1, double num2, double num3){
+        double total = num1+num2+num3;
+        double average = total/3;
+        return average;
+    }
+    
+}
